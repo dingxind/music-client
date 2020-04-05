ee6cb19f138aba7a0e1b1a6679ba3633d5674c98
丁新东 <dingxindong2013@163.com>
2020-04-05 17:12:30 +0800

    初始化 第一次提交

---
+++ src/components/LoginLogo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="login-logo">
+    <div class="item-logo">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-yinle-"></use>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login-logo'
+}
+</script>
+
+<style scoped>
+.login-logo{
+  background-color: #2aa3ef;
+  height: 100vh;
+  width: 700px;
+  overflow: hidden;
+}
+.item-logo {
+  transform: rotate(-30deg);
+}
+.icon {
+  width: 700px;
+  height: 700px;
+  fill: currentColor;
+  color: #2796dd;
+}
+</style>
