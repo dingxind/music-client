5a2b6c5550e44d39c606d2d1ffed057018524050
丁新东 <dingxindong@zhongruigroup.com>
2020-04-17 10:01:42 +0800

    修改

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="swiper">
     <div class="swiper-container">
-      <el-carousel :interval="4000" type="card" height="350px">
+      <el-carousel :interval="4000" type="card" height="280px">
         <el-carousel-item v-for="(item, index) in swiperList" :key="index">
-          <img :src="item.picImg"/>
+          <img class="img" :src="item.picImg"/>
         </el-carousel-item>
       </el-carousel>
     </div>
@@ -12,41 +12,44 @@
 
 <script>
 export default {
-  name: 'swiper',
-  data () {
+  name: "swiper",
+  data() {
     return {
-      swiperList: [{
-        picImg: require('@/assets/img/swiper/blur-1851426_640.jpg')
-      }, {
-        picImg: require('@/assets/img/swiper/concert-768722_640.jpg')
-      }, {
-        picImg: require('@/assets/img/swiper/boy-984293_640.jpg')
-      }, {
-        picImg: require('@/assets/img/swiper/ipad-605439_640.jpg')
-      }, {
-        picImg: require('@/assets/img/swiper/microphone-1209816_640.jpg')
-      }, {
-        picImg: require('@/assets/img/swiper/music-notes-3221097_640.jpg')
-      }, {
-        picImg: require('@/assets/img/swiper/piano-1655558_640.jpg')
-      }, {
-        picImg: require('@/assets/img/swiper/turntable-1337986_640.jpg')
-      }]
-    }
+      swiperList: [
+        {
+          picImg: require("@/assets/img/swiper/1.jpg")
+        },
+        {
+          picImg: require("@/assets/img/swiper/2.jpg")
+        },
+        {
+          picImg: require("@/assets/img/swiper/3.jpg")
+        },
+        {
+          picImg: require("@/assets/img/swiper/4.jpg")
+        },
+        {
+          picImg: require("@/assets/img/swiper/5.jpg")
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style scoped>
-.swiper{
+.swiper {
   width: 60%;
   margin: 40px auto;
 }
 .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-  
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
-  }
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+.img{
+width:100%;
+}
 </style>
